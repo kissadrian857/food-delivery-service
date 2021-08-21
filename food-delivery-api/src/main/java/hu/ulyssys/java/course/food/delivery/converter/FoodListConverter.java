3bb33cef82d397d45f09b7e03ee79e3a1afa4915
Kiss Adrián Imre <kisswadrian857@gmail.com>
2021-08-21 18:18:06 +0200

    Added FoodListConverter class to project

---
+++ food-delivery-api/src/main/java/hu/ulyssys/java/course/food/delivery/converter/FoodListConverter.java
@@ -0,0 +1,38 @@
+package hu.ulyssys.java.course.food.delivery.converter;
+
+import hu.ulyssys.java.course.food.delivery.entity.Food;
+import hu.ulyssys.java.course.food.delivery.service.FoodService;
+
+import javax.inject.Inject;
+import javax.persistence.AttributeConverter;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.StringJoiner;
+import java.util.StringTokenizer;
+
+public class FoodListConverter implements AttributeConverter<List<Food>, String> {
+    @Inject
+    FoodService foodService;
+    private final String GROUP_DELIMITER = ",";
+
+    @Override
+    public String convertToDatabaseColumn(List<Food> foods) {
+        StringJoiner stringJoiner = new StringJoiner(GROUP_DELIMITER);
+        for (Food food : foods) {
+            stringJoiner.add(food.getId().toString());
+        }
+
+        return stringJoiner.toString();
+    }
+
+    @Override
+    public List<Food> convertToEntityAttribute(String s) {
+        List<Food> foods = new ArrayList<>();
+        StringTokenizer st = new StringTokenizer(s, GROUP_DELIMITER);
+        while (!st.hasMoreTokens()) {
+            Long id = Long.parseLong(st.nextToken());
+            foods.add(foodService.findById(id));
+        }
+        return foods;
+    }
+}
