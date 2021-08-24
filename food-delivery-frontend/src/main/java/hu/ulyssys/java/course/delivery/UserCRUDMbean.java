c25bf5be72203d7435c65860a01a9409d23b2487
Kiss Adrián Imre <kisswadrian857@gmail.com>
2021-08-24 20:39:46 +0200

    Added user.xhtml to project and annotated UserCRUDMbean class

--- food-delivery-frontend/src/main/java/hu/ulyssys/java/course/delivery/UserCRUDMbean.java
+++ food-delivery-frontend/src/main/java/hu/ulyssys/java/course/delivery/UserCRUDMbean.java
@@ -4,8 +4,12 @@ package hu.ulyssys.java.course.delivery;
 import hu.ulyssys.java.course.food.delivery.entity.User;
 import hu.ulyssys.java.course.food.delivery.service.UserService;
 
+import javax.faces.view.ViewScoped;
 import javax.inject.Inject;
+import javax.inject.Named;
 
+@Named
+@ViewScoped
 public class UserCRUDMbean extends CoreCRUDMbean<User> {
     @Inject
     public UserCRUDMbean(UserService service) {
